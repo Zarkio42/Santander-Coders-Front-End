483a1875f491198f9e56308ab5434680ad42290b
zarkio42 <pedro.contato24@gmail.com>
2024-08-19 23:19:34 -0300

    main finalizada - ajustes no header (layout e js)

--- headerBehavior.js
+++ headerBehavior.js
@@ -3,8 +3,10 @@ const header = document.querySelector('header');
 function checkScroll() {
   if (window.scrollY > 70) {
     header.classList.add('sticky');
+    header.classList.add('background-black-navbar')
   } else {
     header.classList.remove('sticky');
+    header.classList.remove('background-black-navbar')
   }
 }
 
